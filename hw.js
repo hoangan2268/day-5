9f78f30c04bd4e2f7015f6f16c7e2245ec3752e4
hoangan2268 <92203264+hoangan2268@users.noreply.github.com>
2021-10-20 02:29:25 +0700

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,44 @@
+BAI 1: 
+// let people= prompt('How many People are there?')
+// let pizza= prompt('How many Pizza(s) are there?')
+// let slices= Number(pizza) * (8)
+// let average= Number(slices) / Number (people)
+// alert ('There are ' + people + ' People and ' + pizza + ' Pizzas. Total slices are ' + slices )
+// alert ('Each Person can have an average of ' + average + ' Pizza slice(s). ')
+// console.log('People: ' + people)
+// console.log('Pizza(s): ' + pizza)
+// console.log('Slices: ' + slices)
+// console.log('Average: ' + average)
+
+BAI 2:
+// let temp=prompt('What is the temperature today?')
+// if(temp > 40){
+//     alert('Dangerous weather!')
+// }else if(temp > 25 ){
+//     alert('The weather is hot!')
+// }else if(temp > 15){
+//     alert('The weather is nice!')
+// }else if(temp >0){
+//     alert('The weather is cold!')
+// }else if(temp >-12){
+//     alert('The weather is super cold!')
+// }else if(temp < -12){
+//     alert('Dangerous weather!')
+// }
+
+BAI 3:
+// let name=prompt('What is your name?')
+// for (let i=0; i<10; i++){
+// console.log(name)
+// }
+
+BAI 4 & 5
+// let books=prompt('How many books do you want to import?')
+// const bookshelf = []
+// while(books > 0){
+//     books--
+//     let bookname=prompt('What is the book name?')
+//     console.log(bookname)
+//     bookshelf.push(bookname)
+// }
+// alert('The bookself have: ' + bookshelf)
